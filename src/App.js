d9a905b4487dd43c289bb1a6e8b2593aa9380c8a
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 17:31:44 -1000

    Add routes for react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import VendingMachine from "./VendingMachine";
+import Soda from "./Soda";
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Route exact path="/">
+					<VendingMachine />
+				</Route>
+				<Route exact path="/soda">
+					<Soda />
+				</Route>
+				<Route exact path="/chips">
+					<Chips />
+				</Route>
+				<Route exact path="/sardines">
+					<Sardines />
+				</Route>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
