c5a51f4aa75a17b0a8ea35d99ddb7d2d9b159fa9
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 18:18:34 -1000

    Style Soda component

--- src/Soda.js
+++ src/Soda.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import sodaSrc from "./Soda.png";
+import "./Soda.css";
 
 const Soda = () => {
 	return (
-		<div>
-			<p>OMG Sugar</p>
+		<div className="Soda">
+			<div className="Soda-message">
+				<img src={sodaSrc} alt="soda can" />
+				<p>OMG Sugar</p>
+				<img src={sodaSrc} alt="soda can" />
+			</div>
 			<Link to="/">Go Home</Link>
 		</div>
 	);
