242e6831b9a90bc270ad9aac4e9d84f834112fbb
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 17:33:05 -1000

    Create Chips

---
+++ src/Chips.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Chips = () => {
+	return (
+		<div>
+			<p>Here are some chips.</p>
+			<Link to="/">Go Home</Link>
+		</div>
+	);
+};
+
+export default Chips;
