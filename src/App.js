a2df3cc19c0d8b0730a353b53d3103b1fc85e5bb
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 17:42:12 -1000

    Add Navbar to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route } from "react-router-dom";
+
 import VendingMachine from "./VendingMachine";
 import Soda from "./Soda";
 import Chips from "./Chips";
 import Sardines from "./Sardines";
+import Navbar from "./Navbar";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
+				<Navbar />
 				<Route exact path="/">
 					<VendingMachine />
 				</Route>
