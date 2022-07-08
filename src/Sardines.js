7679fb568cd653de9409b29682f814273f70b3fb
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 17:34:06 -1000

    Create Sardines

---
+++ src/Sardines.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Sardines = () => {
+	return (
+		<div>
+			<p>You don't eat the sardines, they eat you!</p>
+			<Link to="/">Go Home</Link>
+		</div>
+	);
+};
+
+export default Sardines;
