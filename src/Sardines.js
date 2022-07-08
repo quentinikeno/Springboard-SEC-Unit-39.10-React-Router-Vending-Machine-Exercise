65fabb55a25efda45f4597da717182601921bb9b
Quentin Ikeno <ikenoq@gmail.com>
2022-07-07 18:39:56 -1000

    Add styles for Sardines

--- src/Sardines.js
+++ src/Sardines.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "./Sardines.css";
 
 const Sardines = () => {
 	return (
-		<div>
+		<div className="Sardines">
 			<p>You don't eat the sardines, they eat you!</p>
 			<Link to="/">Go Home</Link>
 		</div>
